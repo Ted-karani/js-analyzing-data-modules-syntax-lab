afe9eb853bbb65d4638df0fa216e6b0fc9436321
Ted karani <ted.karani@student.moringaschool.com>
2026-04-03 16:10:34 +0300

    calendar lab

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+require('datejs')
+function combineUsers(...args){
+   const combinedObject = {
+    users : []
+   };
+   for( let name of args){
+    combinedObject.users.push(...name);
+   }
+   combinedObject.merge_date = Date.parse('today').toString('M/d/yyyy');
 
+ 
+  return combinedObject;
 
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
